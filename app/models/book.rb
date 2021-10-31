7f209afb08fa580d23e9e437ee981ca13660ca7c
Jot singh <jot.chawla.54.js@gmail.com>
2021-10-31 12:29:34 +0530

    Added different Types of Validation on 'Book' model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,11 @@
 class Book < ApplicationRecord
+    validates :fullname, presence: true
+    validates :fullname, length: { in: 3..30 }
+    validates :fullname, uniqueness: true
+    # validates :fullname, format: { with: /^[a-zA-Z_-]+$/, message: "only letters _ - are allowed"}         # security risk
+    validates :fullname, format: { with: /\A[a-zA-Z_-]+\z/, message: "can only contains: letters _ -"}
+    validates :author, presence: true
+    validates :publisher, presence: true
+    validates :price, presence: true
+    validates :price, numericality: { greater_than_or_equal_to: 0}
 end
