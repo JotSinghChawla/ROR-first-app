31b291a8ba52b5b7861d078ca29540a1bc99642a
Jot singh <jot.chawla.54.js@gmail.com>
2021-11-02 16:30:44 +0530

    Added 'admin login' controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,21 @@
 class AdminController < ApplicationController
   def login
+    if request.post?
+      if params[:username] == "admin" && params[:password] == "password"
+        session[:admin] = "admin"
+        flash[:notice] = "Welcome, admin"
+        redirect_to :controller => "books", :action => "index"
+      else 
+        flash[:notice] = "Invalid username or password"
+        render :action => :login                            # you can write string here too
+        
+      end
+    end
+  end
+
+  def logout
+    session[:admin] = nil
+    flash[:notice] = "You are now Logged out!!!"
+    redirect_to :action => "login"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'admin/login'
+  post 'admin/login'
+  get 'admin/logout'
   resources :companies
   resources :employees
   
